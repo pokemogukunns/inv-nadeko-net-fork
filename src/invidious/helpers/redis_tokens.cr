47ef5dfe4ca5c103da2c58c61ec6f7e759e7e64f
Fijxu <fijxu@nadeko.net>
2024-11-16 12:27:10 -0300

    Overwirte CONFIG.po_token and CONFIG.visitor_data by the tokens stored on redis
    This approach is better to prevent conflicts with the upstream
    repository.

--- src/invidious/helpers/redis_tokens.cr
+++ src/invidious/helpers/redis_tokens.cr
@@ -7,13 +7,19 @@ module Tokens
     @@po_token = REDIS_DB.get("invidious:po_token")
     @@visitor_data = REDIS_DB.get("invidious:visitor_data")
     if !@@po_token.nil? && !@@visitor_data.nil?
-      LOGGER.debug("RefreshTokens: Successfully updated tokens")
+      set_tokens
+      LOGGER.debug("RefreshTokens: Successfully updated po_token and visitor_data")
     else
       LOGGER.warn("RefreshTokens: Tokens are empty!")
     end
     LOGGER.trace("RefreshTokens: Tokens are:")
-    LOGGER.trace("RefreshTokens: po_token: #{@@po_token}")
-    LOGGER.trace("RefreshTokens: visitor_data: #{@@visitor_data}")
+    LOGGER.trace("RefreshTokens: po_token: #{CONFIG.po_token}")
+    LOGGER.trace("RefreshTokens: visitor_data: #{CONFIG.visitor_data}")
+  end
+
+  def set_tokens
+    CONFIG.po_token = @@po_token
+    CONFIG.visitor_data = @@visitor_data
   end
 
   def get_tokens
