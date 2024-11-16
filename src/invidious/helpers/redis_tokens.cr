13e00e674bff6cd3c995083231febfbddda0782a
Fijxu <fijxu@nadeko.net>
2024-11-16 12:10:51 -0300

    Remove server side generated tokens (448007e5ba)

--- src/invidious/helpers/redis_tokens.cr
+++ src/invidious/helpers/redis_tokens.cr
@@ -6,11 +6,11 @@ module Tokens
   def refresh_tokens
     @@po_token = REDIS_DB.get("invidious:po_token")
     @@visitor_data = REDIS_DB.get("invidious:visitor_data")
-	if !@@po_token.nil? && !@@visitor_data.nil?
-		LOGGER.debug("RefreshTokens: Successfully updated tokens")
-	else
-		LOGGER.warn("RefreshTokens: Tokens are empty!")
-	end
+    if !@@po_token.nil? && !@@visitor_data.nil?
+      LOGGER.debug("RefreshTokens: Successfully updated tokens")
+    else
+      LOGGER.warn("RefreshTokens: Tokens are empty!")
+    end
     LOGGER.trace("RefreshTokens: Tokens are:")
     LOGGER.trace("RefreshTokens: po_token: #{@@po_token}")
     LOGGER.trace("RefreshTokens: visitor_data: #{@@visitor_data}")
@@ -26,29 +26,5 @@ module Tokens
 
   def get_visitor_data
     return @@visitor_data
-  end
-
-  def generate_tokens(user : String)
-    po_token = ""
-    visitor_data = ""
-    attempts = 0
-
-    LOGGER.debug("Generating po_token and visitor_data for user: '#{user}'")
-    REDIS_DB.publish("generate-token", "#{user}")
-
-    while REDIS_DB.get("invidious:#{user}:po_token").nil? && REDIS_DB.get("invidious:#{user}:visitor_data").nil?
-      if attempts > 50
-        break
-      end
-      LOGGER.debug("Waiting for tokens to arrive at redis for user: '#{user}'")
-      attempts += 1
-      sleep 250.milliseconds
-    end
-
-    po_token = REDIS_DB.get("invidious:#{user}:po_token")
-    visitor_data = REDIS_DB.get("invidious:#{user}:visitor_data")
-
-    LOGGER.debug("Tokens successfully generated for user: '#{user}'")
-    return {po_token, visitor_data}
   end
 end
